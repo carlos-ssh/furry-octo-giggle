cad5d7713415639a816869ba274c0bec3d188713
Carlos-SSH <carlos.anezeh@gmail.com>
2022-10-11 18:48:24 -0500

    First commit

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,15 @@ module FurryOctoGiggle
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
     config.i18n.default_locale = 'es'
+
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
+
+    config.assets.debug = false
+    config.assets.compile = true
+    config.assets.precompile += %w( *.js *.css )
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
